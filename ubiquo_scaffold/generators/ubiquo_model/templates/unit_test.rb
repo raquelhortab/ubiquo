ce177a516459aec747f5d68caf210cae44c2a29d
Toni Reina <areina0@gmail.com>
2009-07-03 15:42:00 +0200

    Fixed indentation and fixed published_at date filter test

--- ubiquo_scaffold/generators/ubiquo_model/templates/unit_test.rb
+++ ubiquo_scaffold/generators/ubiquo_model/templates/unit_test.rb
@@ -9,7 +9,7 @@ class <%= class_name %>Test < ActiveSupport::TestCase
     end
   end
 
-  <% unless ton.nil? -%>
+  <%- unless ton.nil? -%>
   def test_should_require_<%= ton %>
     assert_no_difference '<%= class_name %>.count' do
       <%= file_name %> = create_<%= file_name %>(:<%= ton %> => "")
@@ -29,35 +29,35 @@ class <%= class_name %>Test < ActiveSupport::TestCase
   end
   <% end -%>
 
-  <% if has_published_at %>
-    def test_should_filter_by_publish_date
-      <%= class_name %>.destroy_all
-      <%= file_name %>_1,<%= file_name %>_2,<%= file_name %>_3 = [
-        create_<%= file_name %>(:published_at => 5.day.ago),
-        create_<%= file_name %>(:published_at => 10.days.ago),
-        create_<%= file_name %>(:published_at => 5.days.from_now),
-      ]
-      
-      assert_equal_set [], <%= class_name %>.filtered_search({:publish_start => 10.day.from_now})
-      assert_equal_set [<%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 3.day.ago})
-      assert_equal_set [<%= file_name %>_1, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 7.day.ago})
-      assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 12.day.ago})
-      
-      assert_equal_set [], <%= class_name %>.filtered_search({:publish_end => 12.day.ago})
-      assert_equal_set [<%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 7.day.ago})
-      assert_equal_set [<%= file_name %>_1, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 3.day.ago})
-      assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 10.day.from_now})
-      
-      assert_equal_set [<%= file_name %>_1], <%= class_name %>.filtered_search({:publish_start => 7.day.ago, :publish_end => 3.day.ago})
-    end
-  <% end %>
+  <%- if has_published_at -%>
+  def test_should_filter_by_publish_date
+    <%= class_name %>.destroy_all
+    <%= file_name %>_1,<%= file_name %>_2,<%= file_name %>_3 = [
+      create_<%= file_name %>(:published_at => 5.day.ago),
+      create_<%= file_name %>(:published_at => 10.days.ago),
+      create_<%= file_name %>(:published_at => 5.days.from_now),
+    ]
+    
+    assert_equal_set [], <%= class_name %>.filtered_search({:publish_start => 10.day.from_now})
+    assert_equal_set [<%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 3.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 7.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 12.day.ago})
+     
+    assert_equal_set [], <%= class_name %>.filtered_search({:publish_end => 12.day.ago})
+    assert_equal_set [<%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 7.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 3.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 10.day.from_now})
+     
+    assert_equal_set [<%= file_name %>_1], <%= class_name %>.filtered_search({:publish_start => 7.day.ago, :publish_end => 3.day.ago})
+  end
+  <%- end -%>
   private
   
   def create_<%= file_name %>(options = {})
     default_options = {
-      <% for attribute in attributes -%>
-        :<%= attribute.name %> => '<%= attribute.default %>', # <%= attribute.type.to_s %>
-      <% end -%>
+      <%- for attribute in attributes -%>
+      :<%= attribute.name %> => '<%= attribute.default %>', # <%= attribute.type.to_s %>
+      <%- end -%>
     }
     <%= class_name %>.create(default_options.merge(options))
   end
