123d8e4a50c72877d2e9838269b709a77f5417b0
Ramon Salvadó <rsalvado@gnuine.com>
2010-03-22 15:06:35 +0100

    Initial version of generator

--- installer/lib/ubiquo/generator.rb
+++ installer/lib/ubiquo/generator.rb
@@ -1,5 +1,23 @@
 module Ubiquo
-  # TODO: Implement the generator class
+  # -- UBIQUO_PLUGINS should be controlled by profile
+  # -- appname must be provided by opts[:app_name]
+  # TODO: exception_recipient must be provided by ? (default?)
+  # TODO: sender_adress must be provided by ? (default?)
+  # -- choosen_adapter must be provided by opts[:database] (sqlite is sqlite3 in tpls)
+  # -- PROBLEM: routes only profile plugins should add to routes.
+  # TODO: IMPROVEMENT RAILS_GEM_VERSION could be set in options
+  # -- :branch parameter in add_plugins should be set using opts[:template]
   class Generator
+
+    attr_reader :opts, :tpl_skeleton, :rails_template
+    
+    def initialize(opts, tpl_skeleton)
+      @opts, @tpl_skeleton = opts, tpl_skeleton
+    end
+
+    def build_rails_template
+      template = ERB.new(File.read(@tpl_skeleton), 0, "%<>")
+      @rails_template = template.result(binding)
+    end
   end
 end
--- installer/test/ubiquo/generator_test.rb
+++ installer/test/ubiquo/generator_test.rb
@@ -2,8 +2,24 @@ require File.dirname(__FILE__) + '/../helper.rb'
 
 class TestGenerator < Test::Unit::TestCase
 
+  def setup
+    @tpl_skeleton = File.join(File.dirname(__FILE__), "../fixtures", "template.erb")
+    @opts = Options.new(['myapp'])
+  end
+  
   def test_should_create_a_new_generator
-    template = 'mygenerator.rb'
-    assert Generator.new(template, Options.new(['myapp']) )
+    assert Generator.new(@opts, @tpl_skeleton)
+  end
+
+  def test_should_be_able_to_build_a_template
+    opts = Options.new(%w[ --edge --sqlite --minimal myapp ])
+    generator = Generator.new(opts, @tpl_skeleton)
+    rails_template = generator.build_rails_template
+    assert_kind_of String, rails_template
+    assert_match 'myapp', rails_template
+    assert_match 'choosen_adapter = "sqlite"', rails_template
+    assert_match 'choosen_plugin_set = "minimal"', rails_template
+#    assert_match 'ubiquo_branch = nil', rails_template
+    require 'pp'; puts rails_template; exit
   end
 end
