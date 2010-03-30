b1eb4ce546ad9181fff7049464fae6253eaeb186
Bernat Foj Capell <bfoj@gnuine.com>
2010-03-30 10:00:24 +0200

    Correctly added mocha for test files isolation

--- ubiquo_versions/test/unit/adapters_test.rb
+++ ubiquo_versions/test/unit/adapters_test.rb
@@ -1,5 +1,4 @@
 require File.dirname(__FILE__) + "/../test_helper.rb"
-require 'mocha'
 
 class UbiquoVersions::AdaptersTest < ActiveSupport::TestCase
   
--- ubiquo_versions/test/unit/versionable_test.rb
+++ ubiquo_versions/test/unit/versionable_test.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + "/../test_helper.rb"
+require 'mocha'
 
 class Ubiquo::VersionableTest < ActiveSupport::TestCase
   
