8cc5fe06972da7977207f8d3241fd66ed89fe964
Ramon Salvadó <rsalvado@gnuine.com>
2010-06-29 16:39:18 +0200

    Fixed tests to work with the new filtered_search [#328 state:resolved]

--- ubiquo_scaffold/generators/ubiquo_model/templates/unit_test.rb
+++ ubiquo_scaffold/generators/ubiquo_model/templates/unit_test.rb
@@ -16,7 +16,7 @@ class <%= class_name %>Test < ActiveSupport::TestCase
       assert <%= file_name %>.errors.on(:<%= ton %>)
     end
   end
-  
+
   test "should filter by <%= ton %>" do
     <%= class_name %>.destroy_all
     <%= file_name %>_1,<%= file_name %>_2,<%= file_name %>_3 = [
@@ -24,8 +24,9 @@ class <%= class_name %>Test < ActiveSupport::TestCase
       create_<%= file_name %>(:<%= ton %> => "try to FinD me"),
       create_<%= file_name %>(:<%= ton %> => "I don't appear"),
     ]
-    
-    assert_equal_set [<%= file_name %>_1,<%= file_name %>_2], <%= class_name %>.filtered_search({:text => "find"})
+
+    params = { "filter_text" => "find" }
+    assert_equal_set [<%= file_name %>_1,<%= file_name %>_2], <%= class_name %>.filtered_search(params)
   end
   <% end -%>
 
@@ -37,22 +38,22 @@ class <%= class_name %>Test < ActiveSupport::TestCase
       create_<%= file_name %>(:published_at => 10.days.ago),
       create_<%= file_name %>(:published_at => 5.days.from_now),
     ]
-    
-    assert_equal_set [], <%= class_name %>.filtered_search({:publish_start => 10.day.from_now})
-    assert_equal_set [<%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 3.day.ago})
-    assert_equal_set [<%= file_name %>_1, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 7.day.ago})
-    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_start => 12.day.ago})
-     
-    assert_equal_set [], <%= class_name %>.filtered_search({:publish_end => 12.day.ago})
-    assert_equal_set [<%= file_name %>_2], <%= class_name %>.filtered_search({:publish_end => 7.day.ago})
-    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2], <%= class_name %>.filtered_search({:publish_end => 3.day.ago})
-    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({:publish_end => 10.day.from_now})
-     
-    assert_equal_set [<%= file_name %>_1], <%= class_name %>.filtered_search({:publish_start => 7.day.ago, :publish_end => 3.day.ago})
+
+    assert_equal_set [], <%= class_name %>.filtered_search({"filter_publish_start" => 10.day.from_now})
+    assert_equal_set [<%= file_name %>_3], <%= class_name %>.filtered_search({"filter_publish_start" => 3.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_3], <%= class_name %>.filtered_search({"filter_publish_start" => 7.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({"filter_publish_start" => 12.day.ago})
+
+    assert_equal_set [], <%= class_name %>.filtered_search({"filter_publish_end" => 12.day.ago})
+    assert_equal_set [<%= file_name %>_2], <%= class_name %>.filtered_search({"filter_publish_end" => 7.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2], <%= class_name %>.filtered_search({"filter_publish_end" => 3.day.ago})
+    assert_equal_set [<%= file_name %>_1, <%= file_name %>_2, <%= file_name %>_3], <%= class_name %>.filtered_search({"filter_publish_end" => 10.day.from_now})
+
+    assert_equal_set [<%= file_name %>_1], <%= class_name %>.filtered_search({"filter_publish_start" => 7.day.ago, "filter_publish_end" => 3.day.ago})
   end
   <%- end -%>
   private
-  
+
   def create_<%= file_name %>(options = {})
     default_options = {
       <%- for attribute in attributes -%>
