0dd6adaae9cb70e4a7cf65c6bce8890fb3dd437a
Ramon Salvadó <rsalvado@gnuine.com>
2010-07-17 11:57:43 +0200

    updated to 0.3.1

--- installer/lib/ubiquo/options.rb
+++ installer/lib/ubiquo/options.rb
@@ -2,43 +2,42 @@ module Ubiquo
   # TODO: Improve banner to inform of UBIQUO_OPTS env var merging
   # TODO: Add git needed params for repo creation on github
   class Options < Hash
-    
+
     attr_reader :opts, :orig_args
-    
+
     def initialize(args)
       @orig_args = args.clone
-      
+
       self[:template] = :stable
       self[:profile]  = :complete
       self[:locale] = :en
       self[:devel]    = false
-      self[:gnuine]   = args.delete('--gnuine') || false
-      self[:database] = self[:gnuine] ? :postgresql : :sqlite
+      self[:database] = :sqlite
       self[:exception_recipient] = "chan@ge.me"
       self[:sender_address] = "chan@ge.me"
-      
-      
+
+
       @opts = OptionParser.new do |o|
-        o.banner = """Usage: #{File.basename($0)} [options] application_name"        
+        o.banner = """Usage: #{File.basename($0)} [options] application_name"
         o.separator "\nSelect a database (defaults to sqlite if not specified):"
-        
+
         suported_databases.each do |db,msg|
           o.on("--#{db.to_s}", msg) { self[:database] = db }
         end
-        
+
         o.separator "\nSelect a template (defaults to stable if not specified):"
-        
+
         suported_templates.each do |tpl, msg|
           o.on("--#{tpl.to_s}", msg) { self[:template] = tpl }
         end
-        
+
         o.separator "\nSelect a profile (defaults to complete if not specified):"
-        
+
         suported_profiles.each do |profile, msg|
           o.on("--#{profile.to_s}", msg) { self[:profile] = profile }
         end
         o.on(
-          "--custom [PLUGINS]", 
+          "--custom [PLUGINS]",
           "Includes minimal template, but you can add extra plugins"
         ) do |plugins|
           self[:profile] = :custom
@@ -60,7 +59,7 @@ module Ubiquo
         o.on("--sender [EMAIL]", "E-mail to use in from.") do |sender|
           self[:sender_address] = sender
         end
-        
+
         o.separator "\nExtra options:"
 
         o.on("--devel", 'For ubiquo developers (ssh acces to github repos)') do
@@ -70,13 +69,13 @@ module Ubiquo
         o.on("-v", '--version', "Displays this gem version.") do
           self[:version] = File.read(File.dirname(__FILE__) + "/../../VERSION").strip
         end
-        
+
         o.on_tail('-h', '--help', 'displays this help and exit') do
           self[:show_help] = true
         end
-        
+
       end
-      
+
       begin
         @opts.parse!(args)
         self[:app_name] = args.shift
@@ -84,13 +83,13 @@ module Ubiquo
         self[:invalid_argument] = e.message
       end
     end
-    
+
     def merge(other)
       self.class.new(@orig_args + other.orig_args)
     end
-    
+
     private
-    
+
     def suported_databases
       {
         :postgresql => "Uses postgresql database.",
@@ -98,14 +97,14 @@ module Ubiquo
         :sqlite     => "Uses sqlite database."
       }
     end
-    
+
     def suported_templates
       {
         :stable => "Follows the latest ubiquo stable branch. Recommended for production.",
         :edge   => "Follows the development branch."
       }
     end
-    
+
     def suported_profiles
       {
         :minimal  => "Includes minimal set of ubiquo plugins.",
@@ -120,6 +119,6 @@ module Ubiquo
         :en => "Selects English."
       }
     end
-    
+
   end
 end
--- installer/ubiquo.gemspec
+++ installer/ubiquo.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{ubiquo}
-  s.version = "0.2.11"
+  s.version = "0.3.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Albert Callarisa", "Bernat Foj", "Eric Garc\303\255a", "Felip Ladr\303\263n", "Antoni Reina", "Ramon Salvad\303\263", "Arnau S\303\241nchez"]
-  s.date = %q{2010-05-31}
+  s.date = %q{2010-07-17}
   s.default_executable = %q{ubiquo}
   s.description = %q{This gem provides a command-line application to make the creation of ubiquo based applications fast and easy.}
   s.email = %q{rsalvado@gnuine.com}
@@ -39,19 +39,19 @@ Gem::Specification.new do |s|
   s.homepage = %q{http://www.ubiquo.me}
   s.rdoc_options = ["--charset=UTF-8"]
   s.require_paths = ["lib"]
-  s.rubygems_version = %q{1.3.6}
+  s.rubygems_version = %q{1.3.7}
   s.summary = %q{command line application for building ubiquo based applications.}
   s.test_files = [
     "test/helper.rb",
-     "test/ubiquo/options_test.rb",
-     "test/ubiquo/generator_test.rb"
+     "test/ubiquo/generator_test.rb",
+     "test/ubiquo/options_test.rb"
   ]
 
   if s.respond_to? :specification_version then
     current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
     s.specification_version = 3
 
-    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<rails>, ["= 2.3.5"])
       s.add_development_dependency(%q<mocha>, [">= 0.9.8"])
       s.add_development_dependency(%q<highline>, [">= 1.5.2"])
