c7bbd120d7c1c5102cc04435eefb9f298e4cc5dd
Toni Reina <areina0@gmail.com>
2012-01-11 16:39:54 +0100

    Updated register plugin method, now we don't need add plugin loadpaths because we do this in plugin engine [#674]

--- ubiquo_core/lib/ubiquo/plugin.rb
+++ ubiquo_core/lib/ubiquo/plugin.rb
@@ -1,39 +1,35 @@
 module Ubiquo
   class Plugin
-    
+
     cattr_accessor :registered
-    
+
     self.registered ||= {}
-   
+
     def setting(*args)      
       Ubiquo::Settings.add(args)
     end
 
-    def self.register(name,path,config,&block)
-      self.add_plugin_loadpaths(path,config)
+    def self.register(name, &block)
       if name.present?
         Ubiquo::Settings.create_context(name)
         Ubiquo::Settings.context(name, &block)      
       else
         yield Ubiquo::Settings if block_given
-    
+
       end
       self.registered[name] = name
-   
+
     end
-    
-    def self.add_plugin_loadpaths(path,config)
-      I18n.load_path += Dir.glob(File.join(path, 'config', 'locales', '**', '*.{rb,yml}'))
-    end
+
   end
-  
+
   class PluginSpec
-    
+
     class MissingName < StandardError; end
     class MissingVersion < StandardError; end
 
     attr_accessor :name, :version, :url, :desc, :dependencies
-    
+
     def initialize(&block)
       @url = @desc = ''
       @dependencies = {}
