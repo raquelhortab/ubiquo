2c69c569961f50012d0f97bbbbed87fe9081cb03
Bernat Foj Capell <bfoj@gnuine.com>
2012-01-13 09:07:11 +0100

    ubiquo_scaffold is now a gem and a Rails Engine [#698]

--- ubiquo_scaffold/lib/ubiquo_scaffold.rb
+++ ubiquo_scaffold/lib/ubiquo_scaffold.rb
@@ -1,3 +1,13 @@
-require 'ubiquo_scaffold/version'
-require 'ubiquo_scaffold/translation_updater'
-require 'ubiquo_scaffold/commands'
+module UbiquoScaffold
+
+  class Engine < Rails::Engine
+    config.paths["lib"].autoload!
+    config.autoload_paths << "#{config.root}/install/app/controllers"
+
+    initializer :load_extensions do
+      require 'ubiquo_scaffold/version'
+      require 'ubiquo_scaffold/translation_updater'
+      require 'ubiquo_scaffold/commands'
+    end
+  end
+end
