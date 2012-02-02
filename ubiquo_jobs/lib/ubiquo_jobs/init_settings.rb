e28d77b8ad3945d6d7ff73e3f14e161e0095e456
Jon de Andres <jandres@gnuine.com>
2012-02-02 15:06:28 +0100

    Change the way to register the plugin [#673]

--- ubiquo_jobs/lib/ubiquo_jobs/init_settings.rb
+++ ubiquo_jobs/lib/ubiquo_jobs/init_settings.rb
@@ -1,4 +1,4 @@
-Ubiquo::Plugin.register(:ubiquo_jobs) do |config|
+Ubiquo::Plugin.register(:ubiquo_jobs, :plugin => UbiquoJobs) do |config|
   config.add :job_manager_class, UbiquoJobs::Managers::ActiveManager
   config.add :job_notifier_class, UbiquoJobs::Helpers::Notifier
 end
