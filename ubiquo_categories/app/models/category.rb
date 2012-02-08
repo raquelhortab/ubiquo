2d87edf82d1f9ef0bb81f7b3d7dd242678ce0772
Jon de Andres <jandres@gnuine.com>
2012-02-08 12:38:00 +0100

    Change named_scope in the new rails3 way

--- ubiquo_categories/app/models/category.rb
+++ ubiquo_categories/app/models/category.rb
@@ -1,13 +1,15 @@
 class Category < ActiveRecord::Base
-  
+
   belongs_to :category_set
   has_many :category_relations
   belongs_to :parent, :class_name => 'Category'
   has_many :children, :class_name => 'Category', :foreign_key => 'parent_id'
-  
+
   validates_presence_of :name, :category_set
 
-  named_scope :category_set, lambda {|value| {:conditions => {:category_set_id => value}}}
+  scope :category_set, lambda {|value|
+    where(:category_set_id => value)
+  }
 
   filtered_search_scopes :text => [:name], :enable => [:category_set]
 
@@ -30,7 +32,7 @@ class Category < ActiveRecord::Base
   def to_s
     name
   end
-  
+
   def self.alias_for_association association_name
     connection.table_alias_for "#{table_name}_#{association_name}"
   end
