daee68121788d933cdf0cb8ac7c1cdda7ea9f4d8
Juan Hernández <jhernandez@gnuine.com>
2012-02-27 08:40:17 +0100

    scaffold: moved engine declaration to main lib file, following the conventions. [#731 state:resolved]

--- ubiquo_scaffold/lib/ubiquo_scaffold.rb
+++ ubiquo_scaffold/lib/ubiquo_scaffold.rb
@@ -1,12 +1,12 @@
 # -*- encoding: utf-8 -*-
 
-module UbiquoScaffold
-  def self.valid_rails?
-    defined?(Rails) && Rails::VERSION::MAJOR == 3
-  end
+require 'rails'
+require 'ubiquo_scaffold/generators'
 
-  if valid_rails?
-    require 'ubiquo_scaffold/generators'
-    require 'ubiquo_scaffold/engine'
+module UbiquoScaffold
+  class Engine < Rails::Engine
+    engine_name :ubiquo_scaffold
+
+    load_tasks
   end
 end
