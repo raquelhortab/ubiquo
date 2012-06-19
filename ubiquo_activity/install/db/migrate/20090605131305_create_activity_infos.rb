e34b31bd1cad05d41d07d0055cbcfabbae7c7a58
David Lozano <dlozano@gnuine.com>
2012-06-19 10:07:09 +0200

    Added some index to migration

--- ubiquo_activity/install/db/migrate/20090605131305_create_activity_infos.rb
+++ ubiquo_activity/install/db/migrate/20090605131305_create_activity_infos.rb
@@ -1,16 +1,18 @@
 class CreateActivityInfos < ActiveRecord::Migration
   def self.up
     create_table :activity_infos do |t|
-      t.integer :ubiquo_user_id
-      t.string :controller
-      t.string :action
-      t.string :status
+      t.integer :ubiquo_user_id, :index => true
+      t.string :controller, :index => true
+      t.string :action, :index => true
+      t.string :status, :index => true
       t.text :info
       t.integer :related_object_id
       t.string :related_object_type
 
       t.timestamps
+      
     end
+    add_index :activity_infos, [:related_object_id, :related_object_type], :name => :index_activity_infos_on_related_object
   end
 
   def self.down
