77ad7c4045d6519354dc4744ec279942f2237f1e
Bernat Foj Capell <bfoj@gnuine.com>
2012-07-30 09:16:33 +0200

    Reload model instance after destroy its translations. This ensures no caching is taken into account when propagating destroy dependencies (imported from 0.8-stable)

--- ubiquo_i18n/lib/ubiquo_i18n/extensions/active_record.rb
+++ ubiquo_i18n/lib/ubiquo_i18n/extensions/active_record.rb
@@ -226,7 +226,7 @@ module UbiquoI18n
 
           define_method 'destroy_content' do
             self.translations.each(&:destroy)
-            self.destroy
+            self.reload.destroy
           end
 
         end
