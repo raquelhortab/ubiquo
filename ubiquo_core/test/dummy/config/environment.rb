adf6d4e8188c12dec5fbeb33d71e6ac5a1f61f2e
Bernat Foj Capell <bfoj@gnuine.com>
2012-09-21 11:30:05 +0200

    When running tests, install and migrate possible files from other gems. This avoids having to copy migration files or other needed, installable files between gems just for test purposes

--- ubiquo_core/test/dummy/config/environment.rb
+++ ubiquo_core/test/dummy/config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Dummy::Application.initialize!
+
+Dummy::Application.load_tasks
+Rake::Task['ubiquo:install'].invoke
+Rake::Task['db:create'].invoke
+ActiveRecord::Migrator.migrate File.expand_path("../db/migrate/", __FILE__)
