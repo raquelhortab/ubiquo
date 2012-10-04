eeb3b52d61269afe1ae87cd4d59b5c59031f675d
Jon de Andres <jandres@gnuine.com>
2012-10-04 11:48:46 +0200

    Fix path for the migrations files

--- ubiquo_core/test/dummy/config/environment.rb
+++ ubiquo_core/test/dummy/config/environment.rb
@@ -7,4 +7,4 @@ Dummy::Application.initialize!
 Dummy::Application.load_tasks
 Rake::Task['ubiquo:install'].invoke
 Rake::Task['db:create'].invoke
-ActiveRecord::Migrator.migrate File.expand_path("../db/migrate/", __FILE__)
+ActiveRecord::Migrator.migrate File.expand_path("../../db/migrate/", __FILE__)
