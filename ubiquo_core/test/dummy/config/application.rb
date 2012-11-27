6e5b61fda202fda3fae6f38d785c1f0c05ac1d11
Bernat Foj Capell <bfoj@gnuine.com>
2012-11-27 15:18:32 +0100

    Clean test environment before firing tests to avoid interferences from other plugins

--- ubiquo_core/test/dummy/config/application.rb
+++ ubiquo_core/test/dummy/config/application.rb
@@ -15,5 +15,22 @@ module Dummy
 
     # Default in Rails 3.2.3, so it should be supported
     config.active_record.whitelist_attributes = true
+
+    # Remove any previous initializer and other files that can have been placed
+    # in other ubiquo gems testing
+    initializer :clean_environment, :before => :load_config_initializers do
+
+      # delete initializers (except unified_init.rb)
+      to_delete = Dir[File.expand_path('../initializers', __FILE__) + '/*.rb']
+      FileUtils.rm to_delete.reject{|f| f =~ /unified_init/}
+
+      # delete all files in app (the needed ones will be reinstalled)
+      # leave application_XXX (helper or controller) since then are installed by rails
+      app_to_delete = Dir[File.expand_path('../../app/', __FILE__)+ '/**/*.rb']
+      FileUtils.rm app_to_delete.reject{|f| f =~ /application_/}
+
+      # delete all migrations (the needed ones will be reinstalled)
+      FileUtils.rm Dir[File.expand_path('../../db/migrate', __FILE__)+ '/**/*.rb']
+    end
   end
 end
--- ubiquo_core/test/dummy/config/initializers/unified_init.rb
+++ ubiquo_core/test/dummy/config/initializers/unified_init.rb
@@ -1,3 +1,7 @@
+# NOTE: This is the only allowed file in this directory.
+# If you add other files, they will be deleted on test start
+# (see Dummy::Application#clean_initializers)
+
 Dummy::Application.config.session_store :cookie_store, :key => '_dummy_session'
 Dummy::Application.config.secret_token = '37caa61635c9284dce932b122d3751722dae7b3b9beaf6d98377cdfc1a9660b15264a2447d16ec55128ca2be57992afa8e191614668d39718ca821504addf5e3'
 
